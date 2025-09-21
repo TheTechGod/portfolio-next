624b34c5018641ee179de40a5a236f28c046b39d
TheTechGod <gmetzger1911@gmail.com>
2025-09-21 02:58:32 -0500

    Pin Tailwind to v3 and use classic PostCSS; fix Vercel build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
-﻿// tailwind.config.js
-module.exports = {
+﻿module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
