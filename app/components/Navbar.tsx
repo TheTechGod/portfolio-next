bdb9f5679f201512c14b06467a3a6ca4fbce1d36
TheTechGod <gmetzger1911@gmail.com>
2025-09-21 11:46:13 -0500

    Remove react-bootstrap; use plain Bootstrap navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,33 @@
 "use client";
 import Link from "next/link";
-import { Navbar, Nav, Container } from "react-bootstrap";
 
 export default function SiteNavbar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Link href="/" className="navbar-brand">My Portfolio</Link>
-        <Navbar.Toggle aria-controls="main-nav" />
-        <Navbar.Collapse id="main-nav" className="justify-content-end">
-          <Nav>
-            <Link href="/projects" className="nav-link">Projects</Link>
-            <Link href="/blog" className="nav-link">Blog</Link>
-            <Link href="/contact" className="nav-link">Contact</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div className="container">
+        <Link className="navbar-brand" href="/">Integrity Programming</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item"><Link className="nav-link" href="#about">About</Link></li>
+            <li className="nav-item"><Link className="nav-link" href="#projects">Projects</Link></li>
+            <li className="nav-item"><Link className="nav-link" href="#skills">Skills</Link></li>
+            <li className="nav-item"><Link className="nav-link" href="#resume">Resume</Link></li>
+            <li className="nav-item"><Link className="nav-link" href="#contact">Contact</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
+
